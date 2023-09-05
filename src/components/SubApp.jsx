fae5cd04dcf37c838851a1fe7e695b9fb50c65dd
pdoazan <paul.doazan@gmail.com>
2023-09-05 13:01:26 +0200

    first commit

---
+++ src/components/SubApp.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function SubApp({ id, reverse }) {
+    let fullClass = `sub-app sub-${id}`
+    if (reverse) fullClass += ' sub-app-reverse'
+    return (
+        <>
+            <div className={fullClass}>
+                <h1>SubApp #{id}</h1>
+                <div className="container">
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                    <div>hiouphgpiqndvq</div>
+                </div>
+            </div>
+        </>
+    )
+}
