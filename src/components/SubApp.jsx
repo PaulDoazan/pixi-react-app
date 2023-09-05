3332fac5e628a18bacd55cb1738d55a647ab8773
pdoazan <paul.doazan@gmail.com>
2023-09-05 15:17:02 +0200

    Add games in iframe

--- src/components/SubApp.jsx
+++ src/components/SubApp.jsx
@@ -3,42 +3,14 @@ import React from 'react'
 export default function SubApp({ id, reverse }) {
     let fullClass = `sub-app sub-${id}`
     if (reverse) fullClass += ' sub-app-reverse'
+
+    const gameDirectory = id % 2 ? 'towerSlice' : 'fishes'
     return (
         <>
             <div className={fullClass}>
                 <h1>SubApp #{id}</h1>
                 <div className="container">
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
-                    <div>hiouphgpiqndvq</div>
+                    <iframe src={`${process.env.PUBLIC_URL}/games/${gameDirectory}/index.html`}></iframe>
                 </div>
             </div>
         </>
